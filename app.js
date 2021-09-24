b1998202368f832de9e14682fa75e6abc0da21d3
simonp46 <padrongavi@hotmail.com>
2021-09-24 14:24:08 -0500

    funciones java

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
+let Carrito = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+})
+items.addEventListener('clic',e =>{
+    addCarrito(e)
 })
 
 const fetchData = async () => {
@@ -22,9 +26,33 @@ const pintarCards = data => {
         templateCard.querySelector('h5').textContent = producto.title
         templateCard.querySelector('p').textContent = producto.precio
         templateCard.querySelector('img').setAttribute("src",producto.thumbnailUrl)
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id
 
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+}
+
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')){
+        console.log(e.target.parentElement) 
+    }
+    e.stopPropagation()
+}
+const setCarrito = objeto =>{
+     //console.log(objeto)
+     const producto = {
+         id: objeto.querySelector('.btn-dark').dataset.id,
+         title: objeto.querySelector('h5').textContent,
+         precio: objeto.querySelector('p').textContent,
+         cantidad: 1
+     }
+
+     if(Carrito.hasOwnProperty(producto.id)) {
+         producto.cantidad = Carrito[producto.id].cantidad + 1
+     }
+     Carrito[producto.id] = {...producto}
+
+     console.log(producto)
 }
